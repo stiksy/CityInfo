f446a7effd668bfecdcb596708c66e0292c80d00
Fernando Levi Marquardt <live@stiksy.net>
2018-04-25 13:14:50 -0300

    message

---
+++ CityInfo.API/Controllers/CitiesControllers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesControllers : Controller
+    {
+        [HttpGet()]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id=1, Name="Porto Alegre" },
+                new { id=2, Name="Canoas" }
+            });
+        }
+    }
+}
