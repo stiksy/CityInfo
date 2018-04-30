835d3130b09fd86f03d5565eb6236804cc6f1ce8
Fernando Levi Marquardt <live@stiksy.net>
2018-04-30 18:15:13 -0300

    End of module 4

---
+++ CityInfo.API/Models/PointOfInterestForCreationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForCreationDto
+    {
+        [Required(ErrorMessage = "You must provide a name for the point of interest.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
